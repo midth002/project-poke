e51b288dfb6d1e7cbf5626e8e91911896ee2eabc
Andrew Midthun <midth002@gmail.com>
2022-05-31 11:12:42 -0500

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/project_poke',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/project_poke',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
