13b6fa83103134b6567896b17b2caf8585be3ef9
Andrew Midthun <midth002@gmail.com>
2022-05-31 13:01:13 -0500

    Save

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,38 +5,38 @@ import { LOGIN_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
-// const Login = (props) => {
-//     const [formState, setFormState] = useState({ email: '', password: '' });
-//     const [login, { error, data }] = useMutation(LOGIN_USER);
-//     // update state based on form input changes
-//     const handleChange = (event) => {
-//       const { userName, value } = event.target;
-//       setFormState({
-//         ...formState,
-//         [userName]: value,
-//       });
-//     };
+const Login = (props) => {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error, data }] = useMutation(LOGIN_USER);
+    // update state based on form input changes
+    const handleChange = (event) => {
+      const { userName, value } = event.target;
+      setFormState({
+        ...formState,
+        [userName]: value,
+      });
+    };
 
-//   // submit form
-//   const handleFormSubmit = async (event) => {
-//     event.preventDefault();
-//     console.log(formState);
-//     try {
-//       const { data } = await login({
-//         variables: { ...formState },
-//       });
-//       Auth.login(data.login.token);
-//     } catch (e) {
-//       console.error(e);
-//     }
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
 
-//      // clear form values
-//      setFormState({
-//         email: '',
-//         password: '',
-//       });
-//     };
-const Login = () => {
+     // clear form values
+     setFormState({
+        email: '',
+        password: '',
+      });
+    };
+
     return (
         <main className="flex-row justify-center mb-4">
           <div className="col-12 col-lg-10">
