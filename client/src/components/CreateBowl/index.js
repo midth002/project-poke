e202c7bb08dc0c949c520e12d88e4d35da434f5f
Libby Marchetti <lsmarchetti01@gmail.com>
2022-06-01 11:02:03 -0500

    build a bowl

--- client/src/components/CreateBowl/index.js
+++ client/src/components/CreateBowl/index.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {useMutation} from '@apollo/client';
 import { CREATE_BOWL } from '../../utils/mutations';
 import {Modal, Form, Button} from 'react-bootstrap';
+import { storeKeyNameFromField } from '@apollo/client/utilities';
 
 const CreateBowlForm = () => {
     const [showModal, setShow] = useState(false);
@@ -22,23 +23,44 @@ const CreateBowlForm = () => {
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
+
+        try {
+            const {data} = createBowl({
+                variables: {...bowl}
+            });
+            
+            setBowl({
+                size: '',
+                base: '',
+                protein: '',
+                veggies: '',
+                sauces: '',
+                toppings: '',
+            })
+        } catch (e) {
+            console.error(e);
+        }
+
         console.log('added')
-        console.log(bowl)
+        console.log(typeof bowl.size)
         //handleClose();
         
     };
 
-    const handleOrderChange = (event) => {
-        const {data} = createBowl({
-            variables: {...bowl}
-        })
+    const handleFormChange = (event) => {
+        // const {data} = createBowl({
+        //     variables: {...bowl}
+        // })        
 
-        const {name, boolean} = event.target;
+        // setBowl(event.target.value)
+        // console.log(...bowl)
 
-        if (name === 'base' && boolean.true) {
-            setBowl({...bowl, [name]: boolean});
-        } else if (name !==  'base') {
-            //console.log({...bowl})
+        const {name, value} = event.target;
+
+        if (name === 'size' && value.true) {
+            setBowl({...bowl, [name]: value});
+        } else if (name === 'base' && value.true) {
+            setBowl({...bowl, [name]: value});
         }
     }
 
@@ -52,33 +74,28 @@ const CreateBowlForm = () => {
             </Modal.Header>
             <Modal.Body>
                 <Form>
-                <Form.Group onChange={handleOrderChange} className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Label>Size</Form.Label>
-                        <Form.Group className="mb-3">
-                                <Form.Label>Disabled select menu</Form.Label>
-                                <Form.Select>
-                                    <option>Small</option>
-                                    <option>Med</option>
-                                    <option>Lrg</option>
-                                </Form.Select>
-                            </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Label>Size</Form.Label>                        
+                        <Form.Check 
+                        // onClick={() => setBowl(bowl.size='small')}
+                        onChange={(event) => setBowl(event.target.value)} value={bowl.size} name="size" type="checkbox" label="Small" />      
                         <Form.Check type="checkbox" label="Medium" />
                         <Form.Check type="checkbox" label="Large" />                                              
                     </Form.Group>
-                    <Form.Group onChange={handleOrderChange} className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Group onChange={handleFormChange} value={bowl.base} name="base" className="mb-3" controlId="formBasicCheckbox">
                         <Form.Label>Base</Form.Label>
                         <Form.Check value={bowl.base} type="checkbox" label="White Rice" />
                         <Form.Check type="checkbox" label="Brown Rice" />
                         <Form.Check type="checkbox" label="Tofu" />
                         <Form.Check type="checkbox" label="Salad" />                        
                     </Form.Group>
-                    <Form.Group onChange={handleOrderChange} className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Group onChange={handleFormChange} className="mb-3" controlId="formBasicCheckbox">
                         <Form.Label>Protein</Form.Label>
                         <Form.Check value={bowl.protein} type="checkbox" label="Salmon" />
                         <Form.Check type="checkbox" label="Tuna" />
                         <Form.Check type="checkbox" label="Tofu" />                                               
                     </Form.Group>
-                    <Form.Group onChange={handleOrderChange} className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Group onChange={handleFormChange} className="mb-3" controlId="formBasicCheckbox">
                         <Form.Label>Veggies</Form.Label>
                         <Form.Check value={bowl.veggies} type="checkbox" label="Cucumber" />
                         <Form.Check type="checkbox" label="So many" />                                               
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const CREATE_BOWL = gql`
-    mutation createBowl($size: String!, $base: String!, $protein: String!, $veggies: String, $sauces: String, $toppings: String) {
+    mutation createBowl($size: String, $base: String, $protein: String, $veggies: String, $sauces: String, $toppings: String) {
         createBowl(size: $size, base: $base, protein: $protein, veggies: $veggies, sauces: $sauces, toppings: $toppings) {
             _id
             size
@@ -15,25 +15,25 @@ export const CREATE_BOWL = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($userName: String!, $email: String!, $password: String!) {
-    addUser(userName: $userName, email: $email, password: $password) {
-      token
-      user {
-        _id
-        userName
-      }
+    mutation addUser($userName: String!, $email: String!, $password: String!) {
+        addUser(userName: $userName, email: $email, password: $password) {
+        token
+        user {
+            _id
+            userName
+        }
+        }
     }
-  }
-`;
+    `;
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        userName
-      }
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+        token
+        user {
+            _id
+            userName
+        }
+        }
     }
-  }
-`;
+    `;
